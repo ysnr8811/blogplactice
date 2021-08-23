aeef437707cef311d03d972f595acacc64e435cd
ysnr8811 <y.f.arctos9@gmail.com>
2021-08-23 21:44:26 +0900

    ブログの一覧のurl登録

--- blogpost/urls.py
+++ blogpost/urls.py
@@ -1,6 +1,6 @@
-from django.contrib import admin
 from django.urls import path
+from .views import BlogList
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('list/', BlogList.as_view()),
 ]

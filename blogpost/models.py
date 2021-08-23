8ef80755b5be1f4c0661fec2376f2a7887847b3c
ysnr8811 <y.f.arctos9@gmail.com>
2021-08-23 21:23:19 +0900

    マイグレーションの実行

--- blogpost/models.py
+++ blogpost/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class SampleModel(models.Model):
-    title = models.CharField.(max_length=100)
+    title = models.CharField(max_length=100)
     number = models.IntegerField()
 

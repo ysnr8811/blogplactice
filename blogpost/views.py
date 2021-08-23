0580140bce27151e9a1fed5910d2b23afd850674
ysnr8811 <y.f.arctos9@gmail.com>
2021-08-23 21:53:56 +0900

    ブログ一覧画面を呼び出すように修正

--- blogpost/views.py
+++ blogpost/views.py
@@ -1,3 +1,8 @@
+from django.db import models
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import BlogModel
 
-# Create your views here.
+class BlogList(ListView):
+    template_name = 'list.html'
+    model = BlogModel

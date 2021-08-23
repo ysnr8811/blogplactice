82d05c6d5fc4cbf4c1fff1b852f0fe8cb7e42ec5
ysnr8811 <y.f.arctos9@gmail.com>
2021-08-23 22:24:17 +0900

    ブログようのモデルを追加

--- blogpost/models.py
+++ blogpost/models.py
@@ -4,3 +4,12 @@ class SampleModel(models.Model):
     title = models.CharField(max_length=100)
     number = models.IntegerField()
 
+CATEGORY = (('business','ビジネス'),('life','生活'),('other','その他'))
+class BlogModel(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    postdate = models.DateField(auto_now_add=True)
+    category = models.CharField(
+        max_length = 50,
+        choices = CATEGORY
+    )

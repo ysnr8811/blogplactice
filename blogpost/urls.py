c78ba13267a1a84616e875f9a73df9c681514451
ysnr8811 <y.f.arctos9@gmail.com>
2021-08-24 19:33:16 +0900

    新しいurlの登録

--- blogpost/urls.py
+++ blogpost/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BlogList
+from .views import BlogList, BlogDetail
 
 urlpatterns = [
     path('list/', BlogList.as_view()),
+    path('detail/', BlogDetail.as_view()),
 ]

00462616ffdc65285235b138792e3bff0fd470fc
ysnr8811 <y.f.arctos9@gmail.com>
2021-08-24 19:43:28 +0900

    修正

--- blogpost/views.py
+++ blogpost/views.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 from .models import BlogModel
 
 class BlogList(ListView):
     template_name = 'list.html'
     model = BlogModel
 
-class BlogDetail(ListView):
+class BlogDetail(DetailView):
     template_name = 'detail.html'
     model = BlogModel
